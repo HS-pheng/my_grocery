dc00fef80abb0863e0b81184eac4e2b61b650a4c
Hongsreng-P <Hongsreng@mango-byte.com>
2024-02-24 10:06:33 +0700

    second

--- lib/component/main_header.dart
+++ lib/component/main_header.dart
@@ -1,6 +1,5 @@
 // ignore: library_prefixes
 import 'package:badges/badges.dart' as Badge;
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainHeader extends StatelessWidget {
---
+++ lib/view/product/components/product_grid.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_grocery/view/product/components/product_card.dart';
+
+import '../../../model/product.dart';
+
+class ProductGrid extends StatelessWidget {
+  final List<Product> products;
+  const ProductGrid({Key? key, required this.products}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    print(products);
+    // Using a for loop
+    for (int i = 0; i < products.length; i++) {
+      print({
+        'name': products[i].name,
+        'images': products[i].images.first,
+      });
+    }
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 2 / 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (context, index) => ProductCard(product: products[index]),
+    );
+  }
+}
